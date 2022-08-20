1aca829247dce963257700f1f65f907d393b1748
Fernando Soto <fjsoto1199@gmail.com>
2022-08-20 02:43:19 -0500

    Initial commit

---
+++ move_k.py
@@ -0,0 +1,23 @@
+import sys
+
+from AFKMover import AFKMover
+from AFKCommandLineArgParser import AFKCommandLineArgParser
+from AFKCommandLineArgParser import SLEEP_TIME_CODE, STOP_TIME_CODE, SHUTDOWN_DELAY_CODE
+
+
+HELP_KEYWORDS = ('--help', '--h', 'help')
+
+if __name__ == '__main__':
+    args = sys.argv[1:]
+    
+    # if there are args, then assert first is not for help
+    if len(args) == 0 or len(args) > 0 and not args[0] in HELP_KEYWORDS:
+        try:
+            commandLineArgs = AFKCommandLineArgParser(args if args else None)
+
+            p = AFKMover(commandLineArgs)
+            p.start()
+        except (ValueError, KeyError) as ex:
+            print(ex)
+    else:
+        print(f"""py move_k.py [[{SLEEP_TIME_CODE}=sleep_time] [{STOP_TIME_CODE}=hh:mm:ss[AM|PM]] [{SHUTDOWN_DELAY_CODE}=seconds ]]""")
